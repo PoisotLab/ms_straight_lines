8ab94a053decdf6fe3ac8142a63e36f675a500c2
Andrew MacDonald <a.a.m.macdonald@gmail.com>
2019-12-18 16:01:41 -0500

    function to automate converstion to infdata format

---
+++ code/common_functions.jl
@@ -0,0 +1,13 @@
+
+## automate the extraction of a Stan model into an infdata object from Arviz
+function foodweb_model_output(model_chains)
+    const_stan_infdata = from_cmdstan(model_chains,
+        posterior_predictive = "y_hat",
+        log_likelihood = "log_lik",
+        coords = Dict("id" => d.id),
+        dims = Dict(
+            "L" => ["id"],
+            "log_lik" => ["id"],
+            "y_hat" => ["id"]
+    ))
+end
